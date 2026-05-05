93b0472aab00c3b075113bba9a2a2c56ea4fbd4a
ClassicalEngineers8579 <classy.engineers.ftc@gmail.com>
2026-05-05 19:28:29 -0400

    made cool intake subsystem
    its awsum saws
    
    Co-Authored-By: Brian Salva <232259518+Brian-SaI@users.noreply.github.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IntakeSubsystem.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+    DcMotorEx intake;
+
+    public IntakeSubsystem() {
+        intake =  hardwareMap.get(DcMotorEx.class, "Intake");
+    }
+
+    @Override
+    public void periodic() {
+    }
+
+    public void runIntake(double power) {
+        intake.setPower(power);
+    }
+}
